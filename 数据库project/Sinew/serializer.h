8fa4fd47b1e848f95e46809f95048b63abac9d7b
sptzxbbb <sptzxbbb@gmail.com>
2015-06-02 16:25:28 +0800

    Guide

--- 数据库project/Sinew/serializer.h
+++ 数据库project/Sinew/serializer.h
@@ -1,12 +1,15 @@
 #ifndef SERIALIZER_H
 #define SERIALIZER_H
 
-struct serializer {
+struct tuple {
     int attributes_num;
     int *aid; //  n elements
     int *offset;  // (n - 1) elements
     int len;
     char *data;
+
+    struct tuple *child;  // nested_obj
+    int child_CNT;  // number of nested_obj
 }
 
 #endif
