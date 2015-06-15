5efe6e020afa0bddc5d50ef5b32f483602ad86b4
YunAskheart <631038409@qq.com>
2015-06-16 01:52:51 +0800

    test
    fixed string

--- Project/Sinew/test.cpp
+++ Project/Sinew/test.cpp
@@ -5,6 +5,48 @@
 #include "print.h"
 using namespace std;
 
+#define DEBUG
+
+void test_find(catalog& c) {
+  while(1){
+    my_string key_type, key_name;
+    cout << "input key_type"<<endl;
+    cin >> key_type;
+    cout << "input key_name"<<endl;
+    cin >> key_name;
+    cout << "input key_value"<<endl;
+    if (!strcmp(key_type.content(), "int")) {
+      int key_value;
+      cin >> key_value;
+      c.find(key_name.content(), key_value);
+    } else if (!strcmp(key_type.content(), "text")) {
+      my_string key_value;
+      cin >> key_value;
+      c.find(key_name.content(), key_value.content());
+    } else if (!strcmp(key_type.content(), "nested_arr")) {
+      cout << "input strings, end with '|'"<<endl;
+      my_vector<my_string> v;
+      while (1) {
+        my_string a;
+        cin >> a;
+        if (!strcmp(a.content(), "|")) break;
+        v.add(a);
+      }
+//      my_string a("fairly");
+ //     v.add(a);
+      c.find(key_name.content(), v);
+      v.vector_release();
+    } else if (!strcmp(key_type.content(), "bool")) {
+      bool key_value;
+      cin >> key_value;
+      c.find(key_name.content(), key_value);
+    } else if (!strcmp(key_type.content(), "nested_obj")) {
+      cout << "it is too difficult to test!";
+    } else break;
+  }
+}
+
+
 int main(int argc, char *argv[]) {
     FILE* in;
     FILE* out;
@@ -25,6 +67,8 @@ int main(int argc, char *argv[]) {
     fclose(in);
     fclose(out);
 
+
+
     // Transform serial to json
     in = fopen("output", "rb");
     if (NULL == in) {
@@ -40,6 +84,10 @@ int main(int argc, char *argv[]) {
     print(in, out, &CATALOG);
     fclose(in);
     fclose(out);
+
+#ifdef DEBUG
+test_find(CATALOG);
+#endif
 
     return 0;
 }
